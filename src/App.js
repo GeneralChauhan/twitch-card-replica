b7890a794aef725f669710b23dfc2f9e1a457887
Abhinab Pratap Singh Chauhan <abhinabpratap1311@gmail.com>
2022-12-06 14:59:26 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <div class="card">
+        <div class="card-content">
+          <h3 class="card-title">I know exactly what I'm doing</h3>
+        </div>
+        <i class="fa-solid fa-hat-witch card-icon"></i>
+      </div>
+    </div>
+  );
+}
